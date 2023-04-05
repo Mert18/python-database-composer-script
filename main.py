3aed8afd21df7c5547052d8df9c96201b5f4ead3
mert18 <mertuygur02@gmail.com>
2023-04-05 13:39:09 +0300

    refactor(completed_tasks): :snowman: mostly done, completed_tasks table is fulfilled.

--- main.py
+++ main.py
@@ -2,25 +2,28 @@ import psycopg2
 from config import config
 import requests
 import random
+import csv
+from datetime import datetime, timedelta
 
+# for task name
 def generate_random_sentence():
     words = ["apple", "banana", "cat", "dog", "elephant", "frog", "giraffe", "horse", "iguana", "jellyfish", "kangaroo", "lion", "monkey", "noodle", "octopus", "penguin", "quail", "rabbit", "snake", "turtle", "unicorn", "vulture", "whale", "xylophone", "yak", "zebra", "worm"]
     sentence = " ".join(random.sample(words, 3))
     return sentence.capitalize()
 
+# for task difficulty
 def generate_random_difficulty():
     return random.randint(1, 5)
 
-
-
+# connection for database
 conn = None
 try:
-    # read connection parameters
+    # read database connection parameters
     params = config()
     # connect to the PostgreSQL server
     print('Connecting to the PostgreSQL database...')
     conn = psycopg2.connect(**params)	
-    # create a cursor
+    # create a cursor to execute commands
     cur = conn.cursor()
  
     # Add 20 users to "users" table.
@@ -32,21 +35,52 @@ try:
           people = response.json()['results'][0]
           # \" preserves the uppercase letters and required.
           cur.execute('INSERT INTO users(name, email, password) VALUES(%s, %s, %s)', (people['name']['first'] + " " + people['name']['last'], people['email'], people["login"]["md5"]))
-          print("executed once")
  
-    
-    # Populate "tasks" table
+    # Populate "tasks" table with random names and difficulty, and assigneeId from "users" table.
     for i in range(200):
-        cur.execute('INSERT INTO tasks(name, difficulty) VALUES(%s, %s);', (generate_random_sentence(), str(generate_random_difficulty())))
- 
-    # execute a statement
-    # cur.execute('SELECT * FROM users')
-    # results = cur.fetchall()
- 
-    # for row in results:
-    #    print(row)
- 
+        cur.execute('SELECT * FROM users ORDER BY RANDOM() LIMIT 1;')
+        user = cur.fetchone()
+        cur.execute('INSERT INTO tasks(name, difficulty, \"assigneeId\") VALUES(%s, %s, %s);', (generate_random_sentence(), generate_random_difficulty(), user[0]))
+    
+
+    # Populate "completed_tasks" table
+    for i in range(200):
+        cur.execute('SELECT * FROM tasks WHERE status != \'done\' ORDER BY RANDOM() LIMIT 1;')
+        task = cur.fetchone()
+        if(task == None):
+            break
+        
+        with open('./data/task_data.csv', 'r') as file:
+        # Create a csv reader object
+            reader = csv.reader(file)
+
+            # Get a random row from the csv file
+            rows = list(reader)
+            total_rows = len(rows)
+            random_index = random.randint(1, total_rows - 2)
+            random_row = rows[random_index]
+
+            # started date
+            date_format = '%Y-%m-%dT%H:%M:%S.%f%z'
+            date1 = datetime.strptime(random_row[2], date_format)
+
+            # Generate a random number of hours to complete the task: SENSIVITY OF THE DATA to generate the completed_date
+            hours_to_complete = random.randint(task[10] + 4, task[10]*10 + 5)
+            completed_date = date1 + timedelta(hours=hours_to_complete)
+
+            # get the difference in hours - how many hours did it take?
+            delta = completed_date - date1
+            hours = delta.total_seconds() // 3600
+
+            # insert into completed_tasks 
+            cur.execute('INSERT INTO completed_tasks(task_id, user_id, started_date, completed_date, hours) VALUES(%s, %s, %s, %s, %s);', (task[0], task[3], date1, completed_date, hours))
+
+            # update the task status to done
+            cur.execute('UPDATE tasks SET status = \'done\' WHERE id = %s;', (task[0],))
+
+    # commit the changes to the database
     conn.commit()
+    # close the communication with the PostgreSQL
     cur.close()
 except (Exception, psycopg2.DatabaseError) as error:
     print(error)
